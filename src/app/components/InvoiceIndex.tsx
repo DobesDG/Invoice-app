f1b317e9dcb160a6ab3d5711e7d12920df9fe9e4
Douglas Galvao <douglasgalvao06@gmail.com>
2024-10-01 13:51:41 -0300

    Finishes most of main page styling

--- src/app/components/InvoiceIndex.tsx
+++ src/app/components/InvoiceIndex.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import arrow_down from '../public/assets/icon-arrow-down.svg'
 import plus from '../public/assets/icon-plus.svg'
 import DateComponent from "./Date";
+import arrow_right from '../public/assets/icon-arrow-right.svg'
 
 interface Invoice {
     _id: String,
@@ -44,11 +45,11 @@ export const InvoiceIndex: React.FC = () => {
   const colorStatus = (typeStatus: string) => {
     switch (typeStatus) {
       case 'Paid':
-        return ['bg-[#33d69f]','text-[#33d69f]'];
+        return ['bg-light-green','text-light-green','bg-transp-green'];
       case 'Pending':
-        return ['bg-[#ff8f00]','text-[#ff8f00]'];
+        return ['bg-orange','text-orange','bg-transp-orange'];
       case 'Draft':
-        return ['bg-white','text-white'];
+        return ['bg-white','text-white','bg-transp-white'];
       default:
         return '';
     }
@@ -73,14 +74,14 @@ export const InvoiceIndex: React.FC = () => {
     fetchData();
   }, []);
 
-  if (loading) return <p className="flex flex-row w-full justify-center items-center text-white text-xl">Loading...</p>;
-  if (error) return <p className="flex flex-row w-full justify-center items-center text-white text-xl">Error: {error}</p>;
+  if (loading) return <p className="flex flex-row w-full h-[100vh] justify-center items-center text-white text-xl">Loading...</p>;
+  if (error) return <p className="flex flex-row w-full h-[100vh] justify-center items-center text-white text-xl">Error: {error}</p>;
 
   return (
-    <section className="flex flex-col justify-start pt-16 items-center w-full">
-      <section className="flex flex-row text-white gap-72">
+    <section className="flex flex-col justify-start pt-16 items-center w-full tracking-[-0.25px] pl-[103px]">
+      <section className="flex flex-row text-white justify-between w-[730px] mb-14">
         <div className="flex flex-col gap-2">
-          <p className="text-[32px] font-bold">Invoices</p>
+          <p className="text-[32px] font-bold tracking-[-1px]">Invoices</p>
           <p className="text-[12px]">There are {invoices.length} total invoices</p>
         </div>
         <div className="flex flex-row items-center gap-4">
@@ -88,7 +89,7 @@ export const InvoiceIndex: React.FC = () => {
             <p className="text-[12px] font-bold">Filter by status</p>
             <Image className="h-[7px] focus:transform:translate-y-1" src={arrow_down} width={11} alt=""/>
           </button>  
-          <button className="flex flex-row gap-2 justify-start items-center bg-violet pl-2 pr-[15px] py-2 rounded-3xl text-[12px] font-bold hover:bg-light-violet">
+          <button className="flex flex-row gap-4 justify-start items-center bg-violet pl-2 pr-[15px] py-2 rounded-3xl text-[12px] font-bold hover:bg-light-violet">
             <span className="bg-white flex justify-center items-center rounded-3xl w-8 h-8">
               <Image src={plus} alt=""/>
             </span>
@@ -99,21 +100,22 @@ export const InvoiceIndex: React.FC = () => {
       <ul className="flex flex-col w-full justify-center items-center">  
         {invoices.map((invoice, index) => (
           <li key={index}>
-            <div className="flex flex-row bg-dark-blue p-6 rounded-lg mb-4">
+            <div className="flex flex-row bg-dark-blue p-6 rounded-lg mb-4 w-[730px] justify-between">
               <div className="flex flex-row justify-center items-center">
-                <span className="text-[#7e88c3] text-xs font-bold">#</span>
-                <p className="text-white text-xs font-bold">{invoice._id}</p>
+                <span className="text-blue-steel text-xs font-bold">#</span>
+                <p className="text-white text-xs font-bold mr-7">{invoice._id}</p>
                 <p className="text-white text-[0.6875rem]">Due <DateComponent dateString={invoice.date_added}/></p>
               </div>
               <div className="flex flex-row justify-center items-center">
-                <div className="flex flex-row justify-center items-center">
-                  <p className="text-white text-[0.6875rem]">{invoice.pay_to.client_name}</p>
-                  <p className="text-white text-base font-bold">${invoice.item_list.reduce((acc, cur) => acc + cur.quant * cur.price, 0).toFixed(2)}</p>
+                <div className="flex flex-row justify-between items-center w-[249px]">
+                  <p className="text-white text-[0.75rem]">{invoice.pay_to.client_name}</p>
+                  <p className="text-white text-base font-bold">${invoice.item_list.reduce((acc, cur) => acc + cur.quant * cur.price, 0).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}</p>
                 </div>
-                <div className="flex flex-row justify-center items-center">
-                  <span className={`w-[8px] h-[8px] ${colorStatus(invoice.status)[0]} rounded-full`}></span>
-                  <p className={`text-xs font-bold ${colorStatus(invoice.status)[1]}`}>{invoice.status}</p>
+                <div className={`flex flex-row justify-center items-center h-10 w-[6.5rem] rounded-md ml-8 ${colorStatus(invoice.status)[2]}`}>
+                  <span className={`w-[8px] h-[8px] mr-[8px] mb-[1.5px] ${colorStatus(invoice.status)[0]} rounded-full`}></span>
+                  <p className={`text-xs font-bold text-center ${colorStatus(invoice.status)[1]}`}>{invoice.status}</p>
                 </div>
+                  <Image className="ml-4" src={arrow_right} alt="" width={7} height={11} />
               </div>
             </div>
           </li>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,12 @@ const config: Config = {
         'violet' :'#7c5dfa',
         'light-violet':'#9277ff',
         'dark-blue':'#1e2139',
+        'blue-steel':'#7e88c3',
+        'light-green':'#33d69f',
+        'transp-green':'#33d69f0f',
+        'orange':'#ff8f00',
+        'transp-orange':'#ff8f000f',
+        'transp-white':'#dfe3fa0f'
       },
     },
   },
