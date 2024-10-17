68550ca843e6232033612d5adeb8a927a88ca8b1
Douglas Galvao <douglasgalvao06@gmail.com>
2024-10-17 12:52:54 -0300

    Create status updater api route

---
+++ src/app/api/markpaid/[id]/route.ts
@@ -0,0 +1,23 @@
+import { connectDB } from '@/app/api/lib/connectdb';
+import Invoice from '@/app/api/lib/InvoiceSchema';
+
+export const PATCH = async (request: Request, { params }: { params: { id: string } }) => {
+  try {
+    await connectDB();
+    
+    const update = await Invoice.updateOne({ _id: params.id }, {$set: {status: 'Paid'}});
+    
+
+    if (update.modifiedCount === 0) {
+      return new Response("Invoice not found", { status: 404 });
+    }
+
+    return new Response(JSON.stringify({ message: 'Invoice status updated successfully' }), {
+      status: 200,
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+  } catch (error) {
+    return new Response("Failed to update invoice status", { status: 500 });
+  }
+};
